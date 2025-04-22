5537f4e5c7590fab46f92c9d175e44e6a9459671
Brook Vita <bachchinhtran@gmail.com>
2025-04-22 22:49:23 +0700

    fix(rule): not allow null in feed rule -> allow null in feed rule

--- src/main/java/QLNKcom/example/QLNK/DTO/user/UpdateFeedRuleRequest.java
+++ src/main/java/QLNKcom/example/QLNK/DTO/user/UpdateFeedRuleRequest.java
@@ -8,28 +8,22 @@ import lombok.Data;
 
 @Data
 public class UpdateFeedRuleRequest {
-    @NotBlank(message = "Input feed must not be blank")
+
     private String inputFeed;
 
-    @NotNull(message = "Ceiling must not be null")
     @PositiveOrZero(message = "Ceiling must be a positive number")
     private Double ceiling;
 
-    @NotNull(message = "Floor must not be null")
     @PositiveOrZero(message = "Floor must be a positive number")
     private Double floor;
 
-    @NotBlank(message = "Output feed for above ceiling must not be blank")
     private String outputFeedAbove;
 
-    @NotBlank(message = "Output feed for below floor must not be blank")
     private String outputFeedBelow;
 
-    @NotNull(message = "Value for above ceiling must not be blank")
     @PositiveOrZero(message = "AboveValue must be a positive number")
     private Double aboveValue;
 
-    @NotNull(message = "Value for below floor must not be blank")
     @PositiveOrZero(message = "BelowValue must be a positive number")
     private Double belowValue;
 }
